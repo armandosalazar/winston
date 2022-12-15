a674d56b004980799c50a6eb2b76c5e84f2a5b15
Armando Salazar <armandosalazar351@gmail.com>
2022-12-15 01:01:10 -0600

    Remenber forever express.json() please

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const winston = require('winston');
+// const winston = require('winston');
+const logger = require('./utils/logger');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', function(req, res, next) {
-	winston.info('Request to /');
+	// winston.info('Request to /');
+	logger.info('Request to /');
+	logger.warn('War');
+	logger.error('Error');
 	res.json('Works!');
+});
+
+app.post('/logs', function(req, res, next) {
+	console.log('>>>', req.body);
+	res.send('OK');
 });
 
 module.exports = app;
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,37 @@
+// const { default: axios } = require('axios');
 const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf, json, colorize, cli } = format;
+
+module.exports = createLogger({
+  format: combine(
+    // cli(),
+    // colorize({ all: true }),
+    // timestamp(),
+    // colorize(),
+    // label({ label: 'api' }),
+    // json()
+    label({ label: 'right meow!' }),
+    timestamp(),
+    printf(nfo => {
+      // printInfo(nfo);
+      return `${nfo.timestamp} [${nfo.label}] ${nfo.level}: ${nfo.message}`;
+    }),
+  ),
+  transports: [
+    new transports.Console(),
+    // new transports.File({
+    //   filename: 'logs/app.log',
+    // }),
+    new transports.Http({
+      host: 'localhost',
+      port: 3000,
+      path: '/logs',
+    }),
+  ],
+});
+
+
+// function printInfo(nfo) {
+//   console.log(typeof nfo);
+//   axios.post('http://localhost:3000/logs', nfo);
+// }
