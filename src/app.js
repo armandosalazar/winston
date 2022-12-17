1ee76d182daed6cc945891fa30b849dc3652e3ef
Armando Salazar <armandosalazar351@gmail.com>
2022-12-16 23:57:27 -0600

    Add new dependency

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
 // const winston = require('winston');
 const logger = require('./utils/logger');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/', function(req, res, next) {
-	// winston.info('Request to /');
-	logger.info('Request to /');
-	logger.warn('War');
-	logger.error('Error');
-	res.json('Works!');
+app.get('/', function (req, res, next) {
+  // winston.info('Request to /');
+  logger.info('request to /', {
+    meta: {
+      id: 1,
+    },
+  });
+  // logger.warn('War');
+  // logger.error('Error');
+	try {
+		throw new Error('Ha ocurrido un error');
+	} catch (error) {
+		logger.error(error.message);
+		// console.log(error);
+	}
+  res.json('Works!');
 });
 
-app.post('/logs', function(req, res, next) {
-	console.log('>>>', req.body);
-	res.send('OK');
+app.post('/logs', function (req, res, next) {
+  console.log(req.body);
+  res.send('OK');
 });
 
 module.exports = app;
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,31 @@
 // const { default: axios } = require('axios');
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf, json, colorize, cli } = format;
+const { combine, timestamp, label, printf, json, colorize, cli, align } = format;
+
+const colors = {
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  undescore: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hide: '\x1b[8m',
+  fgBlack: '\x1b[30m',
+  fgRed: '\x1b[31m',
+  fgGreen: '\x1b[32m',
+  fgYellow: '\x1b[33m',
+  fgBlue: '\x1b[34m',
+  fgMagenta: '\x1b[35m',
+  fgCyan: '\x1b[36m',
+  fgWhite: '\x1b[37m',
+  bgBlack: '\x1b[40m',
+};
 
 module.exports = createLogger({
+  // level: 'info',
+  defaultMeta: {
+    service: 'login',
+  },
   format: combine(
     // cli(),
     // colorize({ all: true }),
@@ -11,11 +34,17 @@ module.exports = createLogger({
     // label({ label: 'api' }),
     // json()
     label({ label: 'right meow!' }),
-    timestamp(),
-    printf(nfo => {
-      // printInfo(nfo);
-      return `${nfo.timestamp} [${nfo.label}] ${nfo.level}: ${nfo.message}`;
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
     }),
+    // align(),
+    printf((info) => {
+      // printInfo(nfo);
+      return `${colors.fgBlue + info.timestamp + colors.reset} [ ${info.label} ] ${
+        info.level === 'error' ? '\x1b[31m' + info.level + '' : info.level
+      }: ${info.message}`;
+    })
+    // json(),
   ),
   transports: [
     new transports.Console(),
@@ -29,7 +58,6 @@ module.exports = createLogger({
     }),
   ],
 });
-
 
 // function printInfo(nfo) {
 //   console.log(typeof nfo);
